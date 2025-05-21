1caffe9f8528ee0bb3396a1df013ad2c6505310c
Ronja <ronja.sonninen@helsinki.fi>
2025-05-21 17:41:22 +0300

    Exercises 3.9-3.11 done

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -6,8 +6,6 @@ app.use(express.json())
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-const cors = require('cors')
-app.use(cors())
 app.use(express.static('dist'))
 
 let persons = [
