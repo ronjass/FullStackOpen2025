b6185404646b677853efef0dec91ee4f227900c2
Ronja <ronja.sonninen@helsinki.fi>
2025-05-21 15:38:42 +0300

    Getting the app to internet

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -8,6 +8,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 const cors = require('cors')
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     {
