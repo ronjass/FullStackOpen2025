abc402564bd5cdb986fce37117fbb8a67c6f5939
Ronja <ronja.sonninen@helsinki.fi>
2025-05-22 14:02:34 +0300

    Starting to use a database

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 const express = require('express')
+const Person = require('./models/person')
 const app = express()
 const morgan = require('morgan')
 
@@ -8,35 +10,16 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 app.use(express.static('dist'))
 
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+let persons = []
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
   
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then((persons) => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -104,7 +87,7 @@ app.post('/api/persons', (request, response) => {
       })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
