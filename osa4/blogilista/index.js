45b701445ab3649b5fefd19ccf572588e6db0596
Ronja <ronja.sonninen@helsinki.fi>
2025-06-02 16:55:18 +0300

    Exercise 4.1 done

---
+++ osa4/blogilista/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const password = process.argv[2]
+
+const mongoUrl = `mongodb+srv://fullstack:${password}@cluster0.akmry8c.mongodb.net/bloglistApp?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.connect(mongoUrl)
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
