f11ac89dd872b601bfb8189465cb59d920ad556b
Ronja <ronja.sonninen@helsinki.fi>
2025-06-05 10:55:47 +0300

    Exercise 4.5 done

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -12,10 +12,29 @@ const totalLikes = (blogs) => {
         return blogs.map((b) => b.likes).reduce((a, b) => a + b)
     }
 }
+
+const favoriteBlog = (blogs) => {
+    if (blogs[0].likes === undefined) {
+        return 0
+    }
+    if (blogs.length === 1) {
+        return blogs[0].likes
+    } else {
+        var mostLikes = blogs[0]
+
+        for (var i = 0; i < blogs.length; i++) {
+            if (blogs[i].likes > mostLikes.likes) {
+                mostLikes = blogs[i]
+            }
+        }
+        return mostLikes.likes
+    }
+}
   
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
 
 
