ad6e331d2da637c898f368c25d38985fc5176ee3
Ronja <ronja.sonninen@helsinki.fi>
2025-06-11 14:02:24 +0300

    Exercise 4.19 done

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 const Blog = require('../models/blog')
 
@@ -15,11 +16,23 @@ blogsRouter.get('/:id', async (request, response) => {
     response.status(404).end()
   }
 })
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
   
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
 
-    const user = await User.findById(body.user)
+    const user = await User.findById(decodedToken.id)
 
     if (!user) {
       return response.status(400).json({ error: 'UserId missing or not valid '})
