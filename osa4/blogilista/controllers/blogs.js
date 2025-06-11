aa824d4ae19748f156ecc077add520a8fbd029be
Ronja <ronja.sonninen@helsinki.fi>
2025-06-11 13:07:43 +0300

    Exercise 4.17 done

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,9 +1,9 @@
 const blogsRouter = require('express').Router()
-const blog = require('../models/blog')
+const User = require('../models/user')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
       response.json(blogs)
 })
 
@@ -19,20 +19,29 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
+    const user = await User.findById(body.user)
+
+    if (!user) {
+      return response.status(400).json({ error: 'UserId missing or not valid '})
+    }
+
     if (!body.title) {
-      return response.status(400).end()
+      return response.status(400).json({ error: 'title missing' })
     }
     if (!body.url) {
-      return response.status(400).end()
+      return response.status(400).json({ error: 'url missing' })
     } else {
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
         likes: body.likes || 0,
+        user: user._id
     })
     try {
       const savedBlog = await blog.save()
+      user.blogs = user.blogs.concat(savedBlog._id)
+      await user.save()
       response.status(201).json(savedBlog)
     } catch (error) {
       next(error)
@@ -46,7 +55,9 @@ blogsRouter.delete('/:id', async (request, response) => {
 })
 
 blogsRouter.put('/:id', async (request, response, next) => {
-  const { title, author, url, likes } = request.body
+  const { title, author, url, likes, user } = request.body
+
+  const userOfBlog = await User.findById(user)
 
   const blog = await Blog.findById(request.params.id)
   if (!blog) {
@@ -57,9 +68,12 @@ blogsRouter.put('/:id', async (request, response, next) => {
   blog.author = author
   blog.url = url
   blog.likes = likes
+  blog.user = userOfBlog._id
 
   try {
     const updatedBlog = await blog.save()
+    userOfBlog.blogs = userOfBlog.blogs.concat(updatedBlog._id)
+    await userOfBlog.save()
     response.json(updatedBlog)
   } catch (error) {
     next(error)
