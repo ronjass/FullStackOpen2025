8abb6bf8653f28dcc449067aa41a9358ad92376d
Ronja <ronja.sonninen@helsinki.fi>
2025-07-19 12:20:54 +0300

    Exercise 5.15 done

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -55,3 +55,34 @@ test('renders all information after clicking show-button', async () => {
     expect(screen.queryByText('likes 0'))
     expect(screen.queryByText('matti'))
 })
+
+test('clicking the like button twice calls event handler twice', async () => {
+    const blog = {
+        title: 'Test',
+        author: 'matti',
+        url: 'test.com',
+        likes: 0,
+        user: { username: 'matti', name: 'matti' },
+    }
+
+    const user = { username: 'matti' }
+
+    const mockHandler = vi.fn()
+
+    render(
+        <Blog 
+        blog={blog}
+        user={user} 
+        updateLikes={mockHandler}
+        handleDelete={() => {}} />
+    )
+
+    const userEventTest = userEvent.setup()
+    const showButton = screen.getByText('show')
+    await userEventTest.click(showButton)
+    const likeButton = screen.getByText('like')
+    await userEventTest.click(likeButton)
+    await userEventTest.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
+})
