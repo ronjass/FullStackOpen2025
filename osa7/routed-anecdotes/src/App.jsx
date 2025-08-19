3c82b35cfcb4b93795295ebee3755887f5ebac47
Ronja <ronja.sonninen@helsinki.fi>
2025-08-19 10:12:18 +0300

    Exercise 7.2 done

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -1,15 +1,28 @@
 import { useState } from 'react'
 
 import {
-  BrowserRouter as Router,
-  Routes, Route, Link
+  Routes, Route, Link,
+  useMatch
 } from 'react-router-dom'
+
+const Anecdote = ({ anecdote }) => {
+  return (
+    <div>
+      <h2>{anecdote.content}</h2>
+      <div>has {anecdote.votes} votes</div> <br/>
+      <div>for more information see <a href='{anecdote.info}'>{anecdote.info}</a></div> <br/>
+    </div>
+  )
+}
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+        <li key={anecdote.id} >
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>)}
     </ul>
   </div>
 )
@@ -118,9 +131,13 @@ const App = () => {
     padding: 5
   }
 
+  const match = useMatch('/anecdotes/:id')
+  const anecdote = match
+    ? anecdotes.find(anecdote => anecdote.id === Number(match.params.id))
+    : null
+
   return (
     <div>
-    <Router>
       <h1>Software anecdotes</h1>
         <div>
           <Link style={padding} to="/">anecdotes</Link>
@@ -132,8 +149,8 @@ const App = () => {
           <Route path="/" element={<AnecdoteList anecdotes={anecdotes}/>} />
           <Route path="/create" element={<CreateNew addNew={addNew} />} />
           <Route path="/about" element={<About />} />
+          <Route path="/anecdotes/:id" element={<Anecdote anecdote={anecdote} />} />
         </Routes>
-    </Router>
 
     <div>
       <Footer />
