58b09c214eceb5df813d43e737af90fb81a4f9bd
Ronja <ronja.sonninen@helsinki.fi>
2025-08-20 11:47:53 +0300

    Exercise 7.7 done

---
+++ osa7/country-hook/src/App.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(
+    {
+      data: null,
+      error: null,
+      status: 'idle'
+    }
+  )
+
+  useEffect(() => {
+    if (name) {
+      axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+      .then(res => {
+        console.log(res)
+        setCountry({ data: res.data, error: null, status: 'resolved' })
+      })
+      .catch(error => {
+        console.log('error')
+        setCountry({ data: null, error, status: 'rejected'})
+      })
+    }
+  }, [name])
+
+  return country
+}
+
+const Country = ({ country }) => {
+  if (country.status === 'idle') {
+    return null
+  }
+
+  if (country.status === 'rejected') {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.data.name.common} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.name}`}/>  
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
